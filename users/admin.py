2e844a36f6c653ef84441f54c61080d456a8df10
evgenia <smallbee_@mail.ru>
2024-03-25 21:38:09 +0300

    create models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from catalog.models import (Category, Product, Seller, Discount, Order, Cashback, Promocode, ProductImage)
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('article', 'name', 'price')
+    search_fields = ('article', 'name', 'price')
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Seller)
+admin.site.register(Discount)
+admin.site.register(Order)
+admin.site.register(Cashback)
+admin.site.register(Promocode)
+admin.site.register(ProductImage)
+
